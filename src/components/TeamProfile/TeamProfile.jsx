57217918598733ce9bc63d89de03bf04e3f70ae3
skdxxb95 <s3992963@student.rmit.edu.au>
2023-01-07 21:57:59 +1000

    Navbar adjustments

--- src/components/TeamProfile/TeamProfile.jsx
+++ src/components/TeamProfile/TeamProfile.jsx
@@ -1,21 +1,26 @@
 import React from 'react'
+import './teamprofile.css'
 
 const TeamProfile = () => {
   return (
-    <div>
-      <h1>Team Profile</h1>
-      <p>
-      Animi quas est eum dolore. Aperiam ex fuga sit esse consequuntur quibusdam nulla. Amet sunt voluptas ducimus aliquid ipsum omnis qui. Debitis qui molestias consequatur placeat non omnis beatae expedita.
-      <br></br>
-      Praesentium neque quasi natus qui voluptas officiis dolor voluptates. Voluptas ad quaerat assumenda qui odit. Doloremque assumenda porro culpa aut et expedita. Totam et voluptas voluptatibus occaecati blanditiis. Repellendus incidunt perferendis consequatur ratione soluta quae voluptas corporis.
-      <br></br>
-      Occaecati nulla molestias voluptas nostrum facilis est deserunt. Placeat pariatur sint natus ab. Ipsam est repellendus maiores quod. Aut perferendis laborum aut.
-      <br></br>
-      Ut adipisci quas ut esse. Aliquid sit ab beatae sed. Dignissimos sunt tempore recusandae earum omnis. Et animi autem velit culpa reprehenderit voluptatum. Et aut natus exercitationem qui. Velit molestias reprehenderit quo et repudiandae.
-      <br></br>
-      Placeat ipsa voluptas minima in laudantium doloribus et. Quasi iusto nisi accusantium amet reprehenderit. Voluptas dolorem repellat debitis ut voluptatem dolores. Maxime amet est eum quis itaque quibusdam eos. Deserunt nemo unde facere fuga enim minima repellat nihil.
-      </p>
-    </div>
+    <section id='teamprofile'>
+      <div className='teamprofile'>
+        <div className="teamprofile-content">
+          <h1>Team Profile</h1>
+          <p>
+          Animi quas est eum dolore. Aperiam ex fuga sit esse consequuntur quibusdam nulla. Amet sunt voluptas ducimus aliquid ipsum omnis qui. Debitis qui molestias consequatur placeat non omnis beatae expedita.
+          <br></br>
+          Praesentium neque quasi natus qui voluptas officiis dolor voluptates. Voluptas ad quaerat assumenda qui odit. Doloremque assumenda porro culpa aut et expedita. Totam et voluptas voluptatibus occaecati blanditiis. Repellendus incidunt perferendis consequatur ratione soluta quae voluptas corporis.
+          <br></br>
+          Occaecati nulla molestias voluptas nostrum facilis est deserunt. Placeat pariatur sint natus ab. Ipsam est repellendus maiores quod. Aut perferendis laborum aut.
+          <br></br>
+          Ut adipisci quas ut esse. Aliquid sit ab beatae sed. Dignissimos sunt tempore recusandae earum omnis. Et animi autem velit culpa reprehenderit voluptatum. Et aut natus exercitationem qui. Velit molestias reprehenderit quo et repudiandae.
+          <br></br>
+          Placeat ipsa voluptas minima in laudantium doloribus et. Quasi iusto nisi accusantium amet reprehenderit. Voluptas dolorem repellat debitis ut voluptatem dolores. Maxime amet est eum quis itaque quibusdam eos. Deserunt nemo unde facere fuga enim minima repellat nihil.
+          </p>
+        </div>
+      </div>
+    </section>
   )
 }
 
