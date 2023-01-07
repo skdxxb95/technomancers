41e3a9bfeec0f40c0b121a1800f6e87013d92d43
skdxxb95 <s3992963@student.rmit.edu.au>
2023-01-07 12:19:03 +1000

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Best from './assets/Best.PNG'
+
+const App = () => {
+  return (
+    <div>
+        <div>Home Page</div>
+        <img src={Best} alt="Technomancers"></img>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/PersonalInfo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PersonalInfo = () => {
+  return (
+    <div>
+        <div>Personal Information</div>
+    </div>
+  )
+}
+
+export default PersonalInfo
