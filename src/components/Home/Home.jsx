dc28d4b6ed5a34d314c59182395c5e655e2a3ae9
skdxxb95 <s3992963@student.rmit.edu.au>
2023-01-08 14:49:22 +1000

    Started adding buttons to each Assignment1

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './home.css'
 import BEST from '../../assets/Best.PNG'
+import {VscGithub} from 'react-icons/vsc'
 
 const Home = () => {
   return (
@@ -17,9 +18,35 @@ const Home = () => {
           <br></br>
           Occaecati nulla molestias voluptas nostrum facilis est deserunt. Placeat pariatur sint natus ab. Ipsam est repellendus maiores quod. Aut perferendis laborum aut.
           Praesentium neque quasi natus qui voluptas officiis dolor voluptates. Voluptas ad quaerat assumenda qui odit. Doloremque assumenda porro culpa aut et expedita. Totam et voluptas voluptatibus occaecati blanditiis. Repellendus incidunt perferendis consequatur ratione soluta quae voluptas corporis.
+          Animi quas est eum dolore. Aperiam ex fuga sit esse consequuntur quibusdam nulla. Amet sunt voluptas ducimus aliquid ipsum omnis qui. Debitis qui molestias consequatur placeat non omnis beatae expedita.
+          <br></br>
+          Praesentium neque quasi natus qui voluptas officiis dolor voluptates. Voluptas ad quaerat assumenda qui odit. Doloremque assumenda porro culpa aut et expedita. Totam et voluptas voluptatibus occaecati blanditiis. Repellendus incidunt perferendis consequatur ratione soluta quae voluptas corporis.
+          <br></br>
+          Occaecati nulla molestias voluptas nostrum facilis est deserunt. Placeat pariatur sint natus ab. Ipsam est repellendus maiores quod. Aut perferendis laborum aut.
+          Praesentium neque quasi natus qui voluptas officiis dolor voluptates. Voluptas ad quaerat assumenda qui odit. Doloremque assumenda porro culpa aut et expedita. Totam et voluptas voluptatibus occaecati blanditiis. Repellendus incidunt perferendis consequatur ratione soluta quae voluptas corporis.
           </p>
         </div>
       </div>
+      <div className='github-links'>
+          <div>
+            Sarah S.
+          </div>
+          <div>
+            Samuel K.
+          </div>
+          <div>
+            Brodie W.
+          </div>
+          <div>
+            Damian W.
+          </div>
+          <div>
+            Ally H.
+          </div>
+          <div>
+            Samuel B.
+          </div>
+        </div>
     </section>
   )
 }
