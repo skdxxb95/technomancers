13888a02d3c22d0bb5b2a2a42e3ef29f9e2da334
Barrinvorg <s3990599@student.rmit.edu.au>
2023-01-15 14:19:11 +1000

    updated section 2 of industry data

--- src/components/IndustryData/IndustryData.jsx
+++ src/components/IndustryData/IndustryData.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './industrydata.css'
+import CHART1 from '../../assets/Chart 1.png'
 
 const IndustryData = () => {
   return (
     <section id='industrydata'>
       <div className='industrydata'>
-        <div className="industrydata-content">
+        <div className="industrydata-content">       
           <h1>Industry Data</h1>
           <h5>Here's our research.</h5>
           {/*------ THIS IS WHERE YOU ENTER CONTENT ------*/}
@@ -21,8 +22,42 @@ Allyson - ICT Business/Systems Analyst (Australian Government - Jobs and Skills,
 Damian - Cyber Security Analyst (Specialisation of ICT Security) (Australian Government - Jobs and Skills, 2022) (Australian Government, 2021)<br></br><br></br>
 
           <br></br>
-          Praesentium neque quasi natus qui voluptas officiis dolor voluptates. Voluptas ad quaerat assumenda qui odit. Doloremque assumenda porro culpa aut et expedita. Totam et voluptas voluptatibus occaecati blanditiis. Repellendus incidunt perferendis consequatur ratione soluta quae voluptas corporis.
-          <br></br>
+          </p>
+          <br></br><br></br>
+          <p>
+          In the following chart you will find the skills most closely link with most if not all Technomancer’s Team member ideal jobs. <br></br>
+          <img src={CHART1}></img>
+        <br></br>
+        Overall, the group's required skill set includes a mix of both General and IT-specific skills, reflecting the diverse responsibilities and requirements of the different ideal jobs. Some skills, such as communication and problem solving, are applicable to a wide range of jobs and not just the Ideal Jobs specifically, while others are more specific to certain roles or industries.
+        <br></br><br></br>
+        <u><b>General Skills:</b></u>
+        <br></br>       
+                <ul>     
+                  <li>Communication</li>
+                  <li>Problem Solving</li>
+                  <li>Writing</li>
+                  <li>Organizing andd Managing Multiple Priorities/Projects</li>
+                  <li>Interpersonal Skills</li>
+                  <li>Ability to collaborate and work in a team environment</li>
+                  <li>Time Management and ability to meet deadlines</li>
+                  <li>Negotiation</li>
+                </ul><br></br>
+         
+          <b>IT-Specific Skills:</b>
+          <ul>
+            <li>Information Security</li>
+            <li>Compliance Software development (frontend and backend languages, .NET framework, C# programming, F#, and VB.NET)</li>
+            <li>IT project management (Scrum, Microsoft Project, and Project Server/SharePoint)</li>
+            <li>Business analysis (MySQL, SQL server and Oracle)</li>
+            <li>Business improvement processes (ITIL)</li>
+            <li>Software asset management (ITAM, SAM)</li>
+            <li>Coding, testing, and automation techniques (Java, Python)</li>
+            <li>Familiarity with specific programming languages and libraries (JavaScript, TypeScript, React, Node.js, Java) </li>
+            <li>Knowledge of healthcare data management standards (HL7, Rhapsody, FHIR)</li>
+            <li>Experience interpreting licensing terms and conditions (ITAM)</li>
+            <li>ITIL, ITAM, and/or SAM training/certification </li>          
+          </ul>
+          <br></br><br></br>        
           Occaecati nulla molestias voluptas nostrum facilis est deserunt. Placeat pariatur sint natus ab. Ipsam est repellendus maiores quod. Aut perferendis laborum aut.
           <br></br>
           Ut adipisci quas ut esse. Aliquid sit ab beatae sed. Dignissimos sunt tempore recusandae earum omnis. Et animi autem velit culpa reprehenderit voluptatum. Et aut natus exercitationem qui. Velit molestias reprehenderit quo et repudiandae.
