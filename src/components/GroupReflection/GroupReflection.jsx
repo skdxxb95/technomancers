414773adaefb42aa6f63ad347e786541027944e8
skdxxb95 <s3992963@student.rmit.edu.au>
2023-01-18 21:29:53 +1000

    Fixed Group Reflection Layout and added my content

--- src/components/GroupReflection/GroupReflection.jsx
+++ src/components/GroupReflection/GroupReflection.jsx
@@ -12,23 +12,58 @@ const GroupReflection = () => {
             {/*------ THIS IS WHERE YOU ENTER CONTENT ------*/}
             {/*------ THIS IS WHERE YOU ENTER CONTENT ------*/}
             <div>
-                <p>
-                Animi quas est eum dolore. Aperiam ex fuga sit esse consequuntur quibusdam nulla. Amet sunt voluptas ducimus aliquid ipsum omnis qui. Debitis qui molestias consequatur placeat non omnis beatae expedita.
-                <br></br>
-                Praesentium neque quasi natus qui voluptas officiis dolor voluptates. Voluptas ad quaerat assumenda qui odit. Doloremque assumenda porro culpa aut et expedita. Totam et voluptas voluptatibus occaecati blanditiis. Repellendus incidunt perferendis consequatur ratione soluta quae voluptas corporis.
-                <br></br>
-                Occaecati nulla molestias voluptas nostrum facilis est deserunt. Placeat pariatur sint natus ab. Ipsam est repellendus maiores quod. Aut perferendis laborum aut.
-                <br></br>
-                Ut adipisci quas ut esse. Aliquid sit ab beatae sed. Dignissimos sunt tempore recusandae earum omnis. Et animi autem velit culpa reprehenderit voluptatum. Et aut natus exercitationem qui. Velit molestias reprehenderit quo et repudiandae.
-                <br></br>
-                Placeat ipsa voluptas minima in laudantium doloribus et. Quasi iusto nisi accusantium amet reprehenderit. Voluptas dolorem repellat debitis ut voluptatem dolores. Maxime amet est eum quis itaque quibusdam eos. Deserunt nemo unde facere fuga enim minima repellat nihil.
-                </p>
+              {/*----- SARAH'S INTRODUCTION CONTENT ----*/}
+              <h3>Sarah S.</h3>
+              <p>
+                Put group reflection here.
+              </p>
+              {/*------ THIS IS WHERE YOU STOP ------*/}
             </div>
+            <div>
+              {/*----- SAMUEL K'S INTRODUCTION CONTENT ----*/}
+              <h3>Samuel K.</h3>
+              <p>
+                Put group reflection here.
+              </p>
+              {/*------ THIS IS WHERE YOU STOP ------*/}
+            </div>
+            <div>
+              {/*----- BRODIE'S INTRODUCTION CONTENT ----*/}
+              <h3>Brodie W.</h3>
+              <p>
+                Put group reflection here.
+              </p>
+              {/*------ THIS IS WHERE YOU STOP ------*/}
+            </div>
+            <div>
+              {/*----- DAMIAN'S INTRODUCTION CONTENT ----*/}
+              <h3>Damian W.</h3>
+              <p>
+                Put group reflection here.
+              </p>
+              {/*------ THIS IS WHERE YOU STOP ------*/}
+            </div>
+            <div>
+              {/*----- ALLY'S INTRODUCTION CONTENT ----*/}
+              <h3>Ally H.</h3>
+              <p>
+                Put group reflection here.
+              </p>
+              {/*------ THIS IS WHERE YOU STOP ------*/}
+            </div>
+            <div>
+              {/*----- SAMUEL B'S INTRODUCTION CONTENT ----*/}
+              <h3>Samuel B.</h3>
+              <p>
+                Overall, the group assignment was a success with active and engaged team members from the start. The team was well-organized and able to work efficiently together. All members were willing to share their ideas and contribute to the project. This helped us to complete the task faster than expected. The team was able to divide the work into smaller chunks and assign them to individual members, which helped to keep everyone on the same page. However, there were some areas for improvement, such as punctuality for meetings. Sometimes team members were late for the meetings, including myself, which caused delays and made it harder for us to stay on schedule. This could be improved by setting a reminder for the meetings and sending out an agenda beforehand, so everyone could come prepared. One surprising aspect was the team's ability to quickly learn and become proficient in new skills, such as React. React is a JavaScript library for building user interfaces, which was new to most of the team members. But the team picked up the library quickly and were able to contribute to a user-friendly interface in a short amount of time. This was a great learning experience for all of us. I thought the GitHub activity log reflected the distribution of work accurately throughout the group. It helped us to keep track of the progress of the project and to identify any issues that needed to be addressed. It also allowed us to see who was contributing the most and who might need additional support. In conclusion, this group assignment was a great learning experience. It taught me the importance of clear communication and organization in group work. Overall, I feel that the group worked well together and was able to complete the assignment successfully. 
+              </p>
+              {/*------ THIS IS WHERE YOU STOP ------*/}
+            </div>
+        </div>
             {/*------ THIS IS WHERE YOU STOP ------*/}
             {/*------ THIS IS WHERE YOU STOP ------*/}
             {/*------ THIS IS WHERE YOU STOP ------*/}
         </div>
-      </div>
     </section>
   )
 }
