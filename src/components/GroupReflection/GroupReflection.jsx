e4bf81e96126a040cc79dad3fa54dcd931067fb1
skdxxb95 <s3992963@student.rmit.edu.au>
2023-01-19 12:20:01 +1000

    Added Group's reflections and added team reflection section

--- src/components/GroupReflection/GroupReflection.jsx
+++ src/components/GroupReflection/GroupReflection.jsx
@@ -15,7 +15,7 @@ const GroupReflection = () => {
               {/*----- SARAH'S INTRODUCTION CONTENT ----*/}
               <h3>Sarah S.</h3>
               <p>
-                Put group reflection here.
+                Team members were willing to get involved and contribute from the beginning. Even organising our team on the Canvas group chat was simple. Our team are a good mix of age groups and existing IT knowledge; and with varying current work and life experience. This mix worked well, and we all have strengths and skills to bring to the team. Scheduling of team meetings with team members on different time zones and differing work and family commitments was one of my concerns but has seemed to work okay. It would have been an improvement to agree to a schedule of meetings ahead of time across the timeline of the assessment and then cancel if not required, rather than trying to schedule the next meeting at the current meeting. The problem with this method was that if someone was missing, they were not there to help schedule the next meeting. Surprisingly, we have all been willing to help each other out and ask if someone needs help. Additionally, we have all been willing to ask for help. This is important when working in a team that workload support is a two street.  
               </p>
               {/*------ THIS IS WHERE YOU STOP ------*/}
             </div>
@@ -23,7 +23,7 @@ const GroupReflection = () => {
               {/*----- SAMUEL K'S INTRODUCTION CONTENT ----*/}
               <h3>Samuel K.</h3>
               <p>
-                Put group reflection here.
+                Working within a group setting can always be difficult, especially since no one of us knew each other prior to this assessment piece. In saying this, our ability to talk through problems, schedule meetings and discuss work requirements was beyond any expectations I personally could have perceived when starting this piece. Our weakest link as a team lay within our collective ability to not make all meetings throughout the weeks, due to work or life commitments. A surprising aspect from our group would have to be that each individual person brought forward their own set of skills and experiences to further strengthen our final work product. In the beginning days of our group being formed, I was under the impression that without a direct leader, nothing would work out, however whether it be from our individual personalities, we managed to cooperate on a good basis by simply rotating speaking roles and just coexisted with each other and this turned into the best option for our team. Meeting new people who were not only friendly and down to earth but also incredibly informative and held a great attention to ensure each person knew exactly what they were doing made this task just that much smoother.
               </p>
               {/*------ THIS IS WHERE YOU STOP ------*/}
             </div>
@@ -43,7 +43,7 @@ const GroupReflection = () => {
               {/*----- DAMIAN'S INTRODUCTION CONTENT ----*/}
               <h3>Damian W.</h3>
               <p>
-                Put group reflection here.
+                Technomancer’s is certainly and eclectic mix of skills, ages and capabilities. However, we worked damn well as a team, broke up the workload, and all got to work on their section, while still offering insight and advice on other sections as questions came up. What was surprising was that we didn’t have a need for single team leader, everything ran well as a democracy, or majority rules if you will. And that allowed everyone to have input on every aspect of the project as we went along, though I think a better understanding of what was required would have helped us to see what was going to be needed in each section. Still, it worked out well. The differing time zones (and even AEDT vs. AEST), as well as work/life commitments certainly made for an interesting organizational problem for meetings. But we worked around it. Learning to use Gitbash and React was cool. They made things easier to track, and showed me where the errors were in my html coding so I could fix them before they were committed to the main, a very useful tool. I am proud of what the team has achieved throughout this assignment, both in terms of knowledge gained and skills learned. And yes, the Git commits are an excellent reflection of the teams work. With all our information and data being uploaded after the sites main design have been established. Means that it will take less time to organize and present the information. Rather then try to build the design around information already present.
               </p>
               {/*------ THIS IS WHERE YOU STOP ------*/}
             </div>
@@ -51,7 +51,7 @@ const GroupReflection = () => {
               {/*----- ALLY'S INTRODUCTION CONTENT ----*/}
               <h3>Ally H.</h3>
               <p>
-                Put group reflection here.
+                Working in a team has been interesting. Not something I have had to do for some time, having only worked with one other person, occasionally two, for many years, and previous to that in a supervisory role. Being able to communicate through MS teams has been very useful. Technology has made it possible for people from both ends of the Earth to get together and collaborate, the only issue being able to find a suitable time for live discussions due to time differences. A team member organising and sending out the invites to meetings was particularly helpful as was having an alert in Calendar which has insured not missing a meeting, assuming one is able to attend that particular meeting. Working with new technology has perhaps made it difficult to get sections added as early as I would have liked but that will improve as the understanding of the technologies does. A team member proficient in the technology who has been willing to share their knowledge has made it possible for us all to add content.  
               </p>
               {/*------ THIS IS WHERE YOU STOP ------*/}
             </div>
@@ -62,6 +62,12 @@ const GroupReflection = () => {
                 Overall, the group assignment was a success with active and engaged team members from the start. The team was well-organized and able to work efficiently together. All members were willing to share their ideas and contribute to the project. This helped us to complete the task faster than expected. The team was able to divide the work into smaller chunks and assign them to individual members, which helped to keep everyone on the same page. However, there were some areas for improvement, such as punctuality for meetings. Sometimes team members were late for the meetings, including myself, which caused delays and made it harder for us to stay on schedule. This could be improved by setting a reminder for the meetings and sending out an agenda beforehand, so everyone could come prepared. One surprising aspect was the team's ability to quickly learn and become proficient in new skills, such as React. React is a JavaScript library for building user interfaces, which was new to most of the team members. But the team picked up the library quickly and were able to contribute to the website in a short amount of time. This was a great learning experience for all of us. I thought the GitHub activity log reflected the distribution of work accurately throughout the group. It helped us to keep track of the progress of the project and to identify any issues that needed to be addressed. It also allowed us to see who was contributing the most and who might need additional support. In conclusion, this group assignment was a great learning experience. It taught me the importance of clear communication and organization in group work. Overall, I feel that the group worked well together and was able to complete the assignment successfully. 
               </p>
               {/*------ THIS IS WHERE YOU STOP ------*/}
+            </div>
+            <div>
+              <h3>Reflections as a Group</h3>
+              <p>
+                ***Group reflection of 400 words here***
+              </p>
             </div>
         </div>
       </div>
